24f13690c6850d57f64b8593cb1bcf7abeae712b
Thuya Myo Nyunt <ty@kny.co>
2015-09-10 11:50:20 +0630

    Remove schema and add collectionYear

--- api/models/candidate.js
+++ api/models/candidate.js
@@ -2,20 +2,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var CandidateSchema = new Schema({
-  "id": String,
-  "name": String,
-  "gender": String,
-  "legislature": String,
-  "national_id": String,
-  "birthdate": Date,
-  "education": Schema.Types.Mixed,
-  "occupation": [String],
-  "nationality_religion": String,
-  "residency": Schema.Types.Mixed,
-  "constituency": Schema.Types.Mixed,
-  "party_id": String,
-  "mother": Schema.Types.Mixed,
-  "father": Schema.Types.Mixed
+  dataCollectionYear: {type: String}
 });
 
 var Candidate = mongoose.model("Candidate", CandidateSchema);
