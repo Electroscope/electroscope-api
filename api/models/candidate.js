739266fb61ff99a83fa604cfd376edbb1c9069cc
Thuya Myo Nyunt <ty@kny.co>
2015-09-10 13:35:27 +0630

    Disable strict at Schema and make getter true

--- api/models/candidate.js
+++ api/models/candidate.js
@@ -2,8 +2,10 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var CandidateSchema = new Schema({
-  dataCollectionYear: {type: String}
-});
+  dataCollectionYear: { type: String }
+}, {strict: false});
+
+CandidateSchema.set('toObject', { getters: true });
 
 var Candidate = mongoose.model("Candidate", CandidateSchema);
 
--- api/models/party.js
+++ api/models/party.js
@@ -3,7 +3,9 @@ var Schema = mongoose.Schema;
 
 var PartySchema = new Schema({
   dataCollectionYear: {type: String}
-});
+}, {strict: false});
+
+PartySchema.set("toObject", { getters: true });
 
 var Party = mongoose.model("Party", PartySchema);
 
