fd4823fa4f9f1c888997efb909b4bc902382d39e
Thuya Myo Nyunt <ty@kny.co>
2015-09-11 20:02:16 +0630

    Move common function to main Class and add maepaysoh sync feature

--- api/handlers/candidate.js
+++ api/handlers/candidate.js
@@ -1,71 +1,22 @@
 var Candidate = $.rootRequire("api/models/candidate");
+var Handler = $.rootRequire("api/handlers");
 
-var CandidateHandler = {
-  create: function (request) {
-    var data = request.data;
-    return new Promise(function (resolve, reject) {
-      (new Candidate(data)).save(function (err, candidate) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(candidate.toObject());
-        }
-      });
-    });
-  },
-  update: function (request) {
-    var query = request.query;
-    var data = request.data;
-    return new Promise(function (resolve, reject) {
-      Candidate.findOneAndUpdate(query, {$set: data}, {new: true}).lean()
-        .exec(function (err, candidate) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(candidate);
-          }
-        });
-    });
-  },
-  get: function (request) {
-    var query = request.query;
-    return new Promise(function (resolve, reject) {
-      Candidate.find(query).lean()
-        .exec(function (err, candidates) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(candidates);
-          }
-        });
-    });
-  },
-  getOne: function (request) {
-    var query = request.query;
-    return new Promise(function (resolve, reject) {
-      Candidate.findOne(query).lean()
-        .exec(function (err, candidate) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(candidate);
-          }
-        });
-    });
-  },
-  remove: function (request) {
-    var query = request.query;
-    return new Promise(function (resolve, reject) {
-      Candidate.remove(query)
-        .exec(function (err) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve({succes: true});
-          }
-        });
-    });
-  }
+var CandidateHandler = new Handler(Candidate);
+
+CandidateHandler.syncWithMaePaySoh = function () {
+  var self = this;
+  return new Promise(function (resolve, reject) {
+    MaePaySohAPI.candidate.getList()
+      .then(function (candidates) {
+        self.create(candidates)
+          .then(function (savedCandidates) {
+            resolve(saveCandidates);
+          })
+          .catch(reject);
+      })
+      .catch(reject);
+  });
 };
+
 
 module.exports = CandidateHandler;
--- api/handlers/party.js
+++ api/handlers/party.js
@@ -1,71 +1,22 @@
 var Party = $.rootRequire("api/models/party");
+var Handler = $.rootRequire("api/handlers");
+var MaePaySohAPI = $.rootRequire("libs/apis/maepaysoh.js");
 
-var PartyHandler = {
-  create: function (request) {
-    var data = request.data;
-    return new Promise(function (resolve, reject) {
-      (new Party(data)).save(function (err, party) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(party.toObject());
-        }
-      });
-    });
-  },
-  update: function (request) {
-    var query = request.query;
-    var data = request.data;
-    return new Promise(function (resolve, reject) {
-      Party.findOneAndUpdate(query, {$set: data}, {new: true}).lean()
-        .exec(function (err, party) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(party);
-          }
-        });
-    });
-  },
-  get: function (request) {
-    var query = request.query;
-    return new Promise(function (resolve, reject) {
-      Party.find(query).lean()
-        .exec(function (err, parties) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(parties);
-          }
-        });
-    });
-  },
-  getOne: function (request) {
-    var query = request.query;
-    return new Promise(function (resolve, reject) {
-      Party.findOne(query).lean()
-        .exec(function (err, party) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(party);
-          }
-        });
-    });
-  },
-  remove: function (request) {
-    var query = request.query;
-    return new Promise(function (resolve, reject) {
-      Party.remove(query)
-        .exec(function (err) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve({succes: true});
-          }
-        });
-    });
-  }
+var PartyHandler = new Handler(Party);
+
+PartyHandler.syncWithMaePaySoh = function () {
+  var self = this;
+  return new Promise(function (resolve, reject) {
+    MaePaySohAPI.party.getList()
+      .then(function (parties) {
+        self.create(parties)
+          .then(function (savedPartites) {
+            resolve(savePartites);
+          })
+          .catch(reject);
+      })
+      .catch(reject);
+  });
 };
 
 module.exports = PartyHandler;
