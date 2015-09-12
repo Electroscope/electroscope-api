e708d50512fc00d4083e2e2238bd7e1ff9a18d17
Thuya Myo Nyunt <ty@kny.co>
2015-09-12 20:12:18 +0630

    Add cross origin

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
+});
+
 // Controller will be here
 var APIControl = require("./controllers/api");
 app.use("/api/v1.0", APIControl);
