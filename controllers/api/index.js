c2e8c672604274d32f83d6e4589bd3d8da659b9e
Thuya Myo Nyunt <ty@kny.co>
2015-09-12 19:46:44 +0630

    Add party controller

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,9 @@ var apiControl = express.Router();
 
 // Controllers Register here
 var candidate = require("./candidate");
-apiControl.use(candidate)
+apiControl.use(candidate);
+
+var party = require("./party");
+apiControl.use(party);
 
 module.exports = apiControl;
