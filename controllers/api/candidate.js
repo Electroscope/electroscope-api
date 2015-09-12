5eed872fd1dce8ea8bf46bb267b6f4b6fcdd9b03
Thuya Myo Nyunt <ty@kny.co>
2015-09-13 00:05:37 +0630

    Controller accept handler object directly

--- controllers/api/candidate.js
+++ controllers/api/candidate.js
@@ -1,4 +1,6 @@
 var Controller = $.rootRequire("controllers");
-var CandidateController = new Controller("candidate", "api/handlers");
+
+var CandidateHandler = $.rootRequire("api/handlers/candidate");
+var CandidateController = new Controller("candidate", CandidateHandler);
 
 module.exports = CandidateController.router;
--- controllers/api/party.js
+++ controllers/api/party.js
@@ -1,4 +1,5 @@
 var Controller = $.rootRequire("controllers");
+var PartyHandler = $.rootRequire("api/handlers/party");
 var PartyController = new Controller("party", "api/handlers");
 
 module.exports = PartyController.router;
