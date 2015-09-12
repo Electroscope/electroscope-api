dcbd604da3efb8921154e6fee6bd14d2ef4439ff
Thuya Myo Nyunt <ty@kny.co>
2015-09-12 19:50:18 +0630

    Fix on testModel

--- test/pre/testmodel.js
+++ test/pre/testmodel.js
@@ -3,10 +3,18 @@ var Schema = mongoose.Schema;
 
 var TestSchema = new Schema({
 
-}, {strict: false});
+}, {
+  id: true,
+  strict: false
+});
 
-TestSchema.set('toObject', { getters: true });
+TestSchema.set('toObject', { 
+  getters: true,
+  transform: function (doc, ret, options) {
+    doc.id = doc._id;
+  }
+});
 
-var TestModel = mongoose.model("Test", TestSchema);
+TestModel = mongoose.model("Test", TestSchema);
 
 module.exports = TestModel;
