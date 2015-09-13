ba89a81ddc5065a4b96750c073660139e4fc8767
Thuya Myo Nyunt <ty@kny.co>
2015-09-13 20:33:18 +0630

    Add vote ads, impement later

---
+++ api/interactors/vote.js
@@ -0,0 +1,8 @@
+var CandidateHandler = $.rootRequire("api/handlers/candidate");
+var MaePaySohAPI = $.rootRequire("libs/apis/maepaysoh");
+
+var Voter = {
+  
+};
+
+module.exports = Voter;
---
+++ controllers/api/vote.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var router = express.Router();
+var Vote = $.rootRequire("api/interactors/vote.js");
+
+
+module.exports = router;
