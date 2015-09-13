05f8bed9236045c99466979de82cd2bc2343e294
Thuya Myo Nyunt <ty@kny.co>
2015-09-13 20:34:19 +0630

    Add route

--- controllers/api/candidate.js
+++ controllers/api/candidate.js
@@ -3,4 +3,24 @@ var Controller = $.rootRequire("controllers");
 var CandidateHandler = $.rootRequire("api/handlers/candidate");
 var CandidateController = new Controller("candidate", CandidateHandler);
 
+CandidateController.router.get("/candidate-groupby", function (req, res, next) {
+  var query = req.query;
+  CandidateHandler.groupbyLegislatureStateDistrict(query)
+    .then(function (result) {
+      res.send(result);
+    }).catch(function (err) {
+      next(err);
+    });
+});
+
+CandidateController.router.get("/candidate-locations", function (req, res, next) {
+  var query = req.query;
+  CandidateHandler.getLocations(query)
+    .then(function (result) {
+      res.send(result);
+    }).catch(function (err) {
+      next(err);
+    });
+});
+
 module.exports = CandidateController.router;
