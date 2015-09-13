75d6f38774de45310099f643d1473d39b81fa115
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-09-13 12:23:32 +0630

    Add Cors Middleware Package and Add it to Server.js Before All Routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var cors = require('cors');
 var app = express();
 var db = require("./config/database");
 
@@ -13,13 +14,10 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.options('*', cors());
+app.use(cors());
 app.use("/", express.static(path.join(__dirname, "public")));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
-  next();
-});
 
 // Controller will be here
 var APIControl = require("./controllers/api");
