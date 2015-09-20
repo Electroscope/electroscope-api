61a9104a58903364c73de309c2dcc2e0c98e3a2c
Thura Hlaing <trhura@gmail.com>
2015-09-21 03:31:06 +0630

    we only need GET method

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 // Controller is just handle of web deliver
 // It create request model on web request
 // and talk to handler (interacter).
-// Then create response model on handler response 
+// Then create response model on handler response
 // and send back to client
 
 var expressRouter = require("express").Router;
@@ -12,39 +12,12 @@ function Controller(name, handler) {
   this.handler = handler;
 
   var controller = this;
-  
+
   this.router.route("/" + name)
     .get(function (req, res, next) {
       // Get all objects
       controller.makeResponse(handler.find, {
         query: req.query
-      }, res, next);
-    })
-    .post(function (req, res, next) {
-      // Object Createtion
-      controller.makeResponse(handler.create, {
-        data: req.body
-      }, res, next);
-    });
-
-  this.router.route("/" + name + "/:id")
-    .get(function (req, res, next) {
-      // Get specific one 
-      controller.makeResponse(handler.findOne, {
-        query: { _id: req.params.id }
-      }, res, next);
-    })
-    .put(function (req, res, next) {
-      // Update specific one 
-      controller.makeResponse(handler.update, {
-        query: { _id: req.params.id },
-        data: req.body
-      }, res, next);
-    })
-    .delete(function (req, res, next) {
-      // Remove specific one
-      controller.makeResponse(handler.remove, {
-        query: { _id: req.params.id },
       }, res, next);
     });
 };
