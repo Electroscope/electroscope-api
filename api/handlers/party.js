c0026f4134c1760ada60a158cdd414f5164af810
Thura Hlaing <trhura@gmail.com>
2015-09-23 16:05:28 +0630

    synced party_records with MPS

--- api/handlers/party.js
+++ api/handlers/party.js
@@ -3,9 +3,11 @@ var Handler = $.rootRequire("api/handlers");
 var MaePaySohAPI = $.rootRequire("libs/apis/maepaysoh.js");
 
 var PartyHandler = new Handler(PartyModel);
-
 PartyHandler.update = null;
 PartyHandler.remove = null;
+
+var mongojs = require("mongojs");
+var db = mongojs('electroscope', ['party_records']);
 
 PartyHandler.syncWithMaePaySoh = function () {
   var handler = this;
@@ -13,21 +15,58 @@ PartyHandler.syncWithMaePaySoh = function () {
     MaePaySohAPI.party.getAll(function (parties) {
       console.log(parties.length + " parties are got!!");
     }).then(function (parties) {
-      parties = parties.map(function(party){
-        party.party_id = party.id;
-        return party;
+      db.collection('party_records').drop(function (){
+	console.log("Dropped Existing Party Collection.");
       });
 
-      handler.create({
-        data: parties
-      }).then(function () {
-        console.log(parties.length
-            + " parties had been saved");
-        resolve(parties);
-      }).catch(reject);
+      var records = [];
+      for (var i = 0; i < parties.length; i++) {
+      	var p = parties[i];
+      	var party = {};
+
+      	party.code = getPartyCode (p['party_name_english']);
+      	party.name = {
+      	  en: p['party_name_english'],
+      	  my: p['party_name']
+      	};
+      	party.seal = p['party_seal'];
+      	party.flag = p['party_flag'];
+	records.push(party);
+      }
+
+      db.collection('party_records').insert(records, function() {
+	console.log("Imported " + records.length + " parties.");
+	resolve(records);
+      });
 
     }).catch(reject);
   });
 };
+
+var getPartyCode = (function () {
+  var party_codes = [];
+
+  return (function (english_name) {
+    var code = english_name.toUpperCase()
+	  .split(' ')
+	  .map(function (x) {
+	    if (typeof x == 'string' && x.length > 1) {
+	      return x[0].match(/[A-Z]/) ? x[0] : '';
+	    }
+	    return null;
+	  })
+	  .join('');
+
+    var sfx = 1;
+    var party_code = code;
+    while (party_codes.indexOf(party_code) != -1) {
+      party_code = code + sfx;
+    }
+
+    party_codes.push(party_code);
+    return party_code;
+  });
+
+})();
 
 module.exports = PartyHandler;
