a6c2ee3da399defca18e99949cb12e73d393d4bc
Thura Hlaing <trhura@gmail.com>
2015-09-24 14:09:23 +0630

    added votes endpoints

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -8,4 +8,7 @@ apiControl.use(candidate);
 var party = require("./party");
 apiControl.use(party);
 
+var vote = require("./vote");
+apiControl.use(vote);
+
 module.exports = apiControl;
--- controllers/api/vote.js
+++ controllers/api/vote.js
@@ -1,6 +1,46 @@
-var express = require("express");
-var router = express.Router();
-var Vote = $.rootRequire("api/interactors/vote.js");
+var Controller = $.rootRequire("controllers");
 
+var VoteHandler = $.rootRequire("api/handlers/vote");
+var VoteController = new Controller("vote", VoteHandler);
 
-module.exports = router;
+VoteController.router.get("/votes/count", function (req, res, next) {
+  var query = req.query;
+  VoteHandler.getCount(query)
+    .then(function (result) {
+      res.send({data: result});
+    }).catch(function (err) {
+      next(err);
+    });
+});
+
+VoteController.router.get("/votes/count/by-party", function (req, res, next) {
+  var query = req.query;
+  VoteHandler.getByPartyCount(query)
+    .then(function (result) {
+      res.send({data: result});
+    }).catch(function (err) {
+      next(err);
+    });
+});
+
+VoteController.router.get("/votes/count/by-parliament", function (req, res, next) {
+  var query = req.query;
+  VoteHandler.getByParliamentCount(query)
+    .then(function (result) {
+      res.send({data: result});
+    }).catch(function (err) {
+      next(err);
+    });
+});
+
+VoteController.router.get("/votes/count/by-state", function (req, res, next) {
+  var query = req.query;
+  VoteHandler.getByStateCount(query)
+    .then(function (result) {
+      res.send({data: result});
+    }).catch(function (err) {
+      next(err);
+    });
+});
+
+module.exports = VoteController.router;
